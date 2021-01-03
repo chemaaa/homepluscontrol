5f58d6ca5412dda794d55076858ada67c55289c6
@chemaaa <187996+chemaaa@users.noreply.github.com>
2021-01-03 20:12:09 +0100

    Bump up version to 0.0.3

--- homepluscontrol/__init__.py
+++ homepluscontrol/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 # Package Version
-version = "0.0.2"
+version = "0.0.3"
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
