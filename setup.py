26aab702aafa573eec05eeb873dcfcce14398f0f
chemaaa <187996+chemaaa@users.noreply.github.com>
2021-03-13 19:14:51 +0100

    Fix license text in setup.py.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
         "Framework :: AsyncIO",
